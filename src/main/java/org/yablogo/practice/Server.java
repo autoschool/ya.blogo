765bc759ce4de587221aef0a3495aad0c4719673
Sukharev Kirill <SukharevKirill@gmail.com>
2014-11-19 00:15:57 +0300

    created emtpy webapp

---
+++ src/main/java/org/yablogo/practice/Server.java
@@ -0,0 +1,36 @@
+package org.yablogo.practice;
+
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
+import ru.qatools.school.switter.service.DatabaseProvider;
+
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+
+/**
+ * Server class.
+ *
+ */
+public class Server extends ResourceConfig {
+
+    public Server() {
+        register(FreemarkerMvcFeature.class);
+
+        register(new DynamicFeature() {
+            @Override
+            public void configure(ResourceInfo resourceInfo, FeatureContext context) {
+                context.register(DatabaseProvider.class);
+            }
+        });
+
+        packages(Server.class.getPackage().getName());
+    }
+}
+
+
+
+
+
+
